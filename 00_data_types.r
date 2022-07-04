063e1a2331d331f857f7f685af20110857aeb597
Gabriel2409 <gabriel.cournelle@gmail.com>
2022-07-04 22:36:35 +0200

    data types

---
+++ 00_data_types.r
@@ -0,0 +1,25 @@
+# integer
+x <- 2L
+typeof(x)
+
+# double
+y <-3
+typeof(y)
+
+# complex
+z <- 3 + 2i
+typeof(z)
+
+# characters
+a <- "a"
+typeof(a)
+
+b <- "ab"
+typeof(b)
+
+# logical
+q1 <- T
+typeof(q1)
+
+q2 <- F
+typeof(q1)
