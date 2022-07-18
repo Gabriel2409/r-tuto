f87d7220a933228e8d1116dae34bbb3786db70f7
Gabriel2409 <gabriel.cournelle@gmail.com>
2022-07-18 18:37:09 +0200

    shiny examples

--- shiny_apps/00_shiny_examples.r
+++ shiny_apps/00_shiny_examples.r
@@ -1,2 +1,22 @@
 library(shiny)
-runApp("00_first_app", display.mode = "showcase")
+runApp("00_first_app", display.mode = "showcase") # same as runExample("01_hello")
+
+runExample("02_text")
+
+runExample("03_reactivity")
+
+runExample("04_mpg")
+
+runExample("05_sliders")
+
+runExample("06_tabsets")
+
+runExample("07_widgets")
+
+runExample("08_html")
+
+runExample("09_upload")
+
+runExample("10_download")
+
+runExample("11_timer")
