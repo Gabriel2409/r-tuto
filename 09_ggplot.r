c263ef2db697f64dfa73602dcea825784871b0f8
Gabriel2409 <gabriel.cournelle@gmail.com>
2022-07-18 14:06:03 +0200

    themes

--- 09_ggplot.r
+++ 09_ggplot.r
@@ -86,3 +86,35 @@ q8 <- ggplot(data = movies, aes(x = CriticRating, y = AudienceRating, color = Ge
 q8 + geom_point(size = 3) + facet_grid(. ~ Year)
 
 q8 + geom_point(aes(size = BudgetMillions)) + geom_smooth() + facet_grid(Genre ~ Year)
+
+## coordinates
+
+q9 <- ggplot(data = movies, aes(x = CriticRating, y = AudienceRating, size = BudgetMillions, color = Genre))
+
+# set limits. Warning message will pop up if some rows containing missing values are removed
+# indeed, it cuts of some data
+q9 + geom_point() + xlim(50, 100) + ylim(50, 100)
+
+# zooming without removing data
+q9 + geom_point() + coord_cartesian(xlim = c(50, 100), ylim = c(50, 100))
+
+
+## themes
+
+q10 <- ggplot(data = movies, mapping = aes(x = BudgetMillions))
+full_hist <- q10 + geom_histogram(binwidth = 10, aes(fill = Genre), color = "black")
+
+full_hist +
+  xlab("Money Axis") +
+  ylab("Nb of movies") +
+  ggtitle("Movie budget distribution") +
+  theme(
+    axis.title.x = element_text(color = "DarkGreen", size = 30),
+    axis.title.y = element_text(color = "Red", size = 30),
+    axis.text.x = element_text(size = 20),
+    axis.text.y = element_text(size = 20),
+    legend.title = element_text(size = 30),
+    legend.text = element_text(size = 20),
+    legend.position = c(0, 1),
+    plot.title = element_text(color = "DarkBlue", size = 40, family = "Courier")
+  )
